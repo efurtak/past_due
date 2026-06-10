db3665a58300285207a9178825c7c3772f69281a
Emil Furtak <emil.furtak@gmail.com>
2026-06-11 00:41:09 +0200

    sets prod settings

--- past_due_app/past_due_project/settings/prod.py
+++ past_due_app/past_due_project/settings/prod.py
@@ -2,6 +2,13 @@ from .base import *
 
 environ.Env.read_env(os.path.join(BASE_DIR, '.env.prod'))
 
+SECRET_KEY = env('SECRET_KEY')
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', 'web']
+
+
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
